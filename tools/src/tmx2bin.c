c541c442d362c3f5b25cf1306bfddd974713f1d6
jani-nykanen <jnyknn@gmail.com>
2021-06-06 10:16:31 +0300

    More stuff to tmx2bin; still in progress

--- tools/src/tmx2bin.c
+++ tools/src/tmx2bin.c
@@ -2,11 +2,95 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "types.h"
 
 
 static char* buffer = NULL;
+
+
+static i32 find_substring(const str keyword) {
+
+    const char* p = strstr(buffer, keyword);
+    if (p == NULL) return -1;
+
+    return (i32)(size_t)(p - buffer);
+}
+
+
+#define BUFFER_LEN 64
+
+
+static i32 get_numeric_param_value(const str key) {
+
+    char cbuffer [64];
+    i32 i;
+    char c;
+
+    i32 pos = find_substring(key);
+    if (pos < 0) return 0;
+
+    pos += strlen(key) + 2;
+
+    i = 0;
+    while ((c = buffer[pos + i]) != '\"' && i < BUFFER_LEN-1) {
+
+        cbuffer[i ++] = c;
+    }
+    cbuffer[i] = '\0';
+
+    return strtol(cbuffer, NULL, 10);
+}
+
+
+static bool parse_layer_data(i32* dbuffer, u32 size) {
+
+    static const str START = "<data encoding=\"csv\">";
+    static const str END = "</data>";
+
+    char cbuffer [BUFFER_LEN];
+
+    i32 startPos = find_substring(START) + strlen(START);
+    i32 endPos = find_substring(END);
+
+    i32 i, j, k;
+    char c;
+
+    if (endPos < 0 || startPos < 0) {
+
+        fprintf(stderr, "Failed to read layer data!\n");
+        return true;
+    }
+
+    j = 0;
+    k = 0;
+    for (i = startPos; i < endPos; ++ i) {
+
+        c = buffer[i];
+        if (c >= '0' && c <= '9') {
+
+            cbuffer[j ++] = c;
+        }
+        else if (j > 0 && c == ',') {
+
+            cbuffer[j] = '\0';
+            dbuffer[k ++] = (i32)strtol(cbuffer, NULL, 10);
+            j = 0;
+        }
+    }
+
+    if (j > 0) {
+
+        cbuffer[j] = '\0';
+        dbuffer[k ++] = (i32)strtol(cbuffer, NULL, 10);
+    }
+
+    return false;
+}
+
+
+#undef BUFFER_LEN
 
 
 static i32 load_file_to_buffer(const str path) {
@@ -46,12 +130,12 @@ static i32 load_file_to_buffer(const str path) {
 }
 
 
-
-
 i32 main(i32 argc, str* argv) {
 
     str output;
-    i32 i;
+    i32 i, j;
+    i32 width, height;
+    i32* tileBuffer = NULL;
 
     if (argc <= 2) {
         
@@ -67,7 +151,22 @@ i32 main(i32 argc, str* argv) {
             return 1;
         }
 
-        printf("%s\n", buffer);
+        width = get_numeric_param_value("width");
+        height = get_numeric_param_value("height");
+
+        tileBuffer = (i32*) calloc(width * height, sizeof(i32));
+        if (tileBuffer == NULL) {
+
+            fprintf(stderr, "Memory allocation error!\n");
+            return 1;
+        }
+
+        if (parse_layer_data(tileBuffer, width*height)) {
+
+            return 1;
+        }
+
+        free(tileBuffer);
     }
 
     return 0;
